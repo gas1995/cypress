ff7a245d05feafdaca36e2dfd6a9a63e25000a90
gas1995 <gas1995@rambler.ru>
2024-08-20 22:37:25 +0300

    first commit

---
+++ cypressMobile.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl : 'http://localhost:3000/',
+    viewportWidth: 360,
+    viewportHeight: 800,
+    setupNodeEvents(on, config) {
+    },
+  },
+});
